fe2c150e554541388543f4ee90dbba66e6c5b787
Guilherme Girardi <guilherme@ardanis.com>
2023-11-30 16:21:59 -0300

    tidy-up and install icon library

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
-<script setup></script>
+<script setup>
+import HomeView from "./views/HomeView.vue"
+</script>
 
 <template>
-  <h1>Hello, Convenia!</h1>
+  <HomeView />
 </template>
 
 <style scoped></style>
